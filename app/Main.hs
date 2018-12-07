5805dc676dbd69aeee40325c068945e4157f379a
Ben Allred <skuttleman@gmail.com>
2018-12-06 17:43:59 -0700

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,34 @@
+module Main where
+import Data.Function
+import Day1 as Day1
+import System.Environment
+
+run :: ([String] -> String) -> Int -> IO String
+run f day = day
+  & show
+  & (\day -> day ++ ".txt")
+  & (++) "resources/day"
+  & readFile
+  & fmap (f . lines)
+
+daySteps :: [( [String] -> String, [String] -> String )]
+daySteps = [( Day1.step1, Day1.step2 )]
+
+dayStep :: [String] -> ( Int, Int )
+dayStep (day:step:_) =
+  ( (read day) - 1, (read step) - 1 )
+
+runner :: ( Int, Int ) -> IO String
+runner ( day, step ) = 
+  let
+    ( f1, f2 ) = daySteps
+      & drop (day - 1)
+      & head
+  in
+    if step == 0 then
+      run f1 1
+    else
+      run f2 2
+
+main :: IO ()
+main = fmap dayStep getArgs >>= runner >>= putStrLn
---
+++ src/Day1.hs
@@ -0,0 +1,9 @@
+module Day1
+    ( step1, step2
+    ) where
+
+step1 :: [String] -> String
+step1 _ = "tbd"
+
+step2 :: [String] -> String
+step2 _ = "tbd"
