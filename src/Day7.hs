24e0de102004706b190ccf1a7e45244d01e5a1f8
Ben Allred <skuttleman@gmail.com>
2018-12-10 19:48:54 -0700

    Day 7 Step 2

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,10 +1,12 @@
 module Day7 (step1, step2) where
+import Data.Char
 import Data.Function
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Utils.Lists
 import Utils.Maps
 import Utils.Regex
+import Utils.Tuples
 
 
 step1 :: [String] -> String
@@ -12,12 +14,18 @@ step1 lines =
   lines
     & fmap dependency
     & foldl dependencyMap Map.empty
-    & completeOrdered "" Set.empty
+    & completeOrdered Set.empty ""
     & reverse
 
 
 step2 :: [String] -> String
-step2 _ = "tbd"
+step2 lines =
+  lines
+    & fmap dependency
+    & foldl dependencyMap Map.empty
+    & timeParallel 5 60 Set.empty 0 Map.empty
+    & show
+
 
 dependency :: String -> ( Char, Char )
 dependency s =
@@ -34,22 +42,38 @@ dependencyMap map ( master, slave ) =
     & updateWith Set.empty (Set.insert master) slave
 
 
-completeOrdered :: String -> Set.Set Char -> Map.Map Char (Set.Set Char) -> String
-completeOrdered sequence completed dependencies =
+completeOrdered :: Set.Set Char -> String -> Map.Map Char (Set.Set Char) -> String
+completeOrdered completed sequence dependencies =
   case nextAvailable completed dependencies of
-    Just next ->
-      completeOrdered (next:sequence) (Set.insert next completed) dependencies
-    Nothing -> sequence
+    [] ->
+      sequence
+    (next:_) ->
+      completeOrdered (Set.insert next completed) (next:sequence) dependencies
 
 
-nextAvailable :: Set.Set Char -> Map.Map Char (Set.Set Char) -> Maybe Char
+nextAvailable :: Set.Set Char -> Map.Map Char (Set.Set Char) -> [Char]
 nextAvailable completed dependencies =
   dependencies
     & Map.toList
     & filter (not . (\key -> Set.member key completed) . fst)
     & filter (diff completed . snd)
     & fmap fst
-    & safeHead
+
+
+withNextAvailable :: Set.Set Char -> Map.Map Char (Set.Set Char) -> Map.Map Int ( Int, Maybe Char ) -> Int -> [Int] -> Map.Map Int ( Int, Maybe Char )
+withNextAvailable completed dependencies inProgress timeOffset ids =
+  let
+    inProgressSteps =
+      inProgress
+        & Map.toList
+        & keep (snd . snd)
+        & Set.fromList
+  in
+    nextAvailable completed dependencies
+      & filter (not . (\char -> Set.member char inProgressSteps))
+      & zip ids
+      & fmap (map2of2 (\char -> ( toTimeNeeded timeOffset char, Just char )))
+      & foldl (\m ( k, v ) -> Map.insert k v m) inProgress
 
 
 diff :: Set.Set Char -> Set.Set Char -> Bool
@@ -57,3 +81,47 @@ diff completed dependents =
   completed
     & Set.difference dependents
     & Set.null
+
+
+timeParallel :: Int -> Int -> Set.Set Char -> Int -> Map.Map Int ( Int, Maybe Char ) -> Map.Map Char (Set.Set Char) -> Int
+timeParallel workers timeOffset completed timer inProgress dependencies
+  | Map.null inProgress =
+    timeParallel workers timeOffset completed timer (Map.fromList (fmap (\id -> ( id, ( 0, Nothing ) )) [1..workers])) dependencies
+  | otherwise =
+    let
+      ( nextCompleted, nextInProgress ) =
+        inProgress
+          & Map.toList
+          & filter ((== 0) . fst . snd)
+          & fmap fst
+          & withNextAvailable completed dependencies inProgress timeOffset
+          & updateTick completed
+    in
+      if Set.size nextCompleted == Map.size dependencies then
+        timer + 1
+      else
+        timeParallel workers timeOffset nextCompleted (timer + 1) nextInProgress dependencies
+
+
+updateTick :: Set.Set Char -> Map.Map Int ( Int, Maybe Char ) -> ( Set.Set Char, Map.Map Int ( Int, Maybe Char ) )
+updateTick completed inProgress =
+  inProgress
+    & Map.toList
+    & foldl collectFinished ( completed, Map.empty )
+
+
+collectFinished :: ( Set.Set Char, Map.Map Int ( Int, Maybe Char ) ) -> ( Int, ( Int, Maybe Char ) ) -> ( Set.Set Char, Map.Map Int ( Int, Maybe Char ) )
+collectFinished ( completed, inProgress ) ( id, ( count, maybeStep ) ) =
+  case maybeStep of
+    Nothing ->
+      ( completed, Map.insert id ( 0, Nothing ) inProgress )
+    Just step ->
+      if count <= 1 then
+        ( Set.insert step completed, Map.insert id ( 0, Nothing ) inProgress )
+      else
+        ( completed, Map.insert id ( count - 1, maybeStep ) inProgress )
+
+
+toTimeNeeded :: Int -> Char -> Int
+toTimeNeeded offset letter =
+  offset + ord letter - (ord 'A' - 1)
--- src/Utils/Lists.hs
+++ src/Utils/Lists.hs
@@ -1,4 +1,4 @@
-module Utils.Lists (foldPairsL, foldPairsR, safeHead) where
+module Utils.Lists (foldPairsL, foldPairsR, keep, safeHead) where
 
 
 foldPairsL :: (a -> b -> b -> a) -> a -> [b] -> a
@@ -9,6 +9,11 @@ foldPairsL f a =
 foldPairsR :: (b -> b -> a -> a) -> a -> [b] -> a
 foldPairsR f a =
   snd . foldr (pairUpR f) ( Nothing, a )
+
+
+keep :: (a -> Maybe b) -> [a] -> [b]
+keep f items =
+  foldr (folder f) [] items
 
 
 safeHead :: [a] -> Maybe a
@@ -23,3 +28,12 @@ pairUpL f ( Nothing, a ) b = ( Just b, a )
 pairUpR :: (b -> b -> a -> a) -> b -> ( Maybe b, a ) -> ( Maybe b, a )
 pairUpR f b ( Just b', a ) = ( Just b, f b' b a )
 pairUpR f b ( Nothing, a ) = ( Just b, a )
+
+
+folder :: (a -> Maybe b) -> a -> [b] -> [b]
+folder f a items =
+  case f a of
+    Nothing ->
+      items
+    Just b ->
+      b:items
